ad30affe7cd5e5d4327c54140e570e849b416c80
Zsomi4 <k72302872172@ktch.hu>
2023-01-12 09:06:30 +0100

    Regisztráció megirása az AuthController-ben.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use App\Models\User;
+use Validator;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
-    //
+    public function signUp(Request $request) {
+        $validator = Validator::make($request->all(),
+        [
+            'name' =>'required',
+            'email' =>'required',
+            'password' =>'required',
+            'confirm_password' =>'required|same:password',
+        ]);
+
+        if($validator->fails()) {
+            // return sendError('Error validator!', $validator->errors());
+            print_r("Error validator!");
+        }
+
+        $input = $request->all();
+        $input["password"] = bcrypt($input["password"]);
+        $user = User::create($input);
+        $success["name"] = $user->name;
+
+        // return $this->sendResponse($success, "Sikeres regisztráció!");
+        print_r("Sikeres regisztráció!");
+    }
 }
